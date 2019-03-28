0d5226ebba8a6696ee867c0d2c37657c12b19bc7
toi.le <toi.le@tbwa.com>
2019-03-28 14:23:19 +0700

    config file  guplfile

--- gulpfile.js
+++ gulpfile.js
@@ -258,7 +258,7 @@ gulp.task('wiredep', () => {
 gulp.task('libs', ['slick'], () => {
   return gulp.src([
     'node_modules/jquery/dist/jquery.min.js',
-    'node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+    'node_modules/bootstrap/dist/js/bootstrap.min.js',
     'node_modules/sweetalert/dist/sweetalert.min.js'
   ])
   .pipe($.plumber())
